86a0723d89d050ed58a90254e6bb567fad869c8d
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2026-07-21 14:20:48 -0600

    init fyne gui start

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	// "fmt"
+	"fyne.io/fyne/v2/app"
+	// "fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	// Setup Fyne GUI boilerplate
+	myApp := app.New()
+	window := myApp.NewWindow("temp")
+	window.ShowAndRun()
+}
