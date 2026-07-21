b1f0d5672e8cbd29d7e497f90c32b3343c2d2416
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2026-07-21 17:36:50 -0600

    add form for adding a newly surrendered animal to add

--- main.go
+++ main.go
@@ -1,17 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	// "fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/widget"
 )
+
+func addSurrenderedAnimal(animalName string, animalTypeString string) {
+	// Do we convert back from string to AnimalType here?
+}
+
+// Create UI element allowing user to add newly surrendered animal to database
+func makeSurrenderForm() fyne.CanvasObject {
+	// Create widgets for name text field and animal type selector dropdown
+	animalName := widget.NewEntry()
+	animalTypeSelector := widget.NewSelect(getAllAnimalStrings(), func(s string) {})
+	animalTypeSelector.PlaceHolder = "Select animal type"
+
+	// Add widgets to a form
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "Animal Name", Widget: animalName, Required: true},
+			{Text: "Animal Type", Widget: animalTypeSelector, Required: true},
+		},
+		// OnCancel will act as clear button
+		OnCancel: func() {
+			animalName.Refresh()
+			animalTypeSelector.Refresh()
+		},
+		// Send off form data to other function to handle database interaction
+		OnSubmit: func() {
+			addSurrenderedAnimal(animalName.Text, animalTypeSelector.Selected)
+		},
+		CancelText: "Clear",
+		SubmitText: "Submit",
+	}
+	form.Refresh()
+	return form
+
+}
+
+func makeAdoptionForm() fyne.CanvasObject {
+	return nil
+}
 
 func main() {
 	// Setup Fyne GUI boilerplate
 	myApp := app.New()
 	window := myApp.NewWindow("Pet Adoption Tracker")
-
-	fmt.Println(Dog)
 
 	window.ShowAndRun()
 }
--- models.go
+++ models.go
@@ -1,12 +1,17 @@
 package main
 
 // Define an enum to represent each animal type
+// NOTE: AnimalType, allAnimalTypes, and animalNames should be updated together
+// when adding/removing animal types
 type AnimalType int
 
 const (
 	Dog AnimalType = iota
 	Cat
 )
+
+// Keep track of all types so we can loop over them
+var allAnimalTypes = []AnimalType{Dog, Cat}
 
 // Allow mapping/conversion from AnimalType to string for printing as string
 var animalNames = map[AnimalType]string{
@@ -17,6 +22,14 @@ var animalNames = map[AnimalType]string{
 // Implement String() for AnimalType for printing
 func (animal AnimalType) String() string {
 	return animalNames[animal]
+}
+
+func getAllAnimalStrings() []string {
+	var animalTypeStrings []string
+	for _, animal := range allAnimalTypes {
+		animalTypeStrings = append(animalTypeStrings, animal.String())
+	}
+	return animalTypeStrings
 }
 
 // Data model
