6a129527e66f2e68f32332dd05590f26ef046035
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2026-07-22 17:45:26 -0600

    display forms for adding/adopting an animal

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func addSurrenderedAnimal(animalName string, animalTypeString string) {
 	// Do we convert back from string to AnimalType here?
+}
+
+func getAdoptedAnimal(animalTypeString string) {
+
 }
 
 // Create UI element allowing user to add newly surrendered animal to database
@@ -23,16 +29,16 @@ func makeSurrenderForm() fyne.CanvasObject {
 			{Text: "Animal Name", Widget: animalName, Required: true},
 			{Text: "Animal Type", Widget: animalTypeSelector, Required: true},
 		},
-		// OnCancel will act as clear button
-		OnCancel: func() {
-			animalName.Refresh()
-			animalTypeSelector.Refresh()
-		},
+		// Cancel will act as clear button
+		// OnCancel: func() {
+		// 	animalName.Refresh()
+		// 	animalTypeSelector.Refresh()
+		// },
 		// Send off form data to other function to handle database interaction
 		OnSubmit: func() {
 			addSurrenderedAnimal(animalName.Text, animalTypeSelector.Selected)
 		},
-		CancelText: "Clear",
+		// CancelText: "Clear",
 		SubmitText: "Submit",
 	}
 	form.Refresh()
@@ -41,13 +47,38 @@ func makeSurrenderForm() fyne.CanvasObject {
 }
 
 func makeAdoptionForm() fyne.CanvasObject {
-	return nil
+	animalTypeSelector := widget.NewSelect(getAllAnimalStrings(), func(s string) {})
+	animalTypeSelector.PlaceHolder = "Select animal type"
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "Animal Type", Widget: animalTypeSelector, Required: true},
+		},
+		// Cancel will act as clear button
+		// OnCancel: func() {
+		// 	animalTypeSelector.Refresh()
+		// },
+		// Send off form data to other function to handle database interaction
+		OnSubmit: func() {
+			getAdoptedAnimal(animalTypeSelector.Selected)
+		},
+		// CancelText: "Clear",
+		SubmitText: "Submit",
+	}
+	form.Refresh()
+	return form
 }
 
 func main() {
 	// Setup Fyne GUI boilerplate
 	myApp := app.New()
 	window := myApp.NewWindow("Pet Adoption Tracker")
+
+	addSurrenderedAnimalSection := makeSurrenderForm()
+	getAdoptedAnimalSection := makeAdoptionForm()
+
+	windowContent := container.New(layout.NewVBoxLayout(), addSurrenderedAnimalSection, getAdoptedAnimalSection)
+	window.SetContent(windowContent)
 
 	window.ShowAndRun()
 }
