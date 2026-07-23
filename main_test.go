caa897e0c1a466a05c3ae5525bd9916d6ff823c2
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2026-07-23 17:46:04 -0600

    add randomized testing for getAnimalCount()

--- main_test.go
+++ main_test.go
@@ -10,8 +10,8 @@ import (
 	_ "modernc.org/sqlite"
 	"regexp"
 	"slices"
+	"strconv"
 	"testing"
-	// "strconv"
 )
 
 // Quick checks to see if AnimalType enum and related helpers are consistent
@@ -288,5 +288,111 @@ func TestGetAdoptedAnimalFailWrongType(t *testing.T) {
 	expectedMsg := "Sorry, but we don't have any animal types to adopt."
 	if resultMsg != expectedMsg {
 		t.Fatalf("Expected message for no animal to adopt, but got: %s", resultMsg)
+	}
+}
+
+// Store intended number of animals per type, for however many types exist for AnimalType
+type CountData struct {
+	countsPerType []int
+}
+
+// Small helper to check total number of animals in countsPerType
+func sumSlice(s []int) int {
+	res := 0
+	for _, val := range s {
+		res += val
+	}
+	return res
+}
+
+// Test getAnimalCount for specific animal type and all animal type
+func TestGetAnimalCount(t *testing.T) {
+	// First, prepare test data based on current number of animal types
+	numTypes := len(allAnimalTypes)
+	// Include all zeroes + randomized counts
+	testData := []CountData{
+		{make([]int, numTypes)},
+	}
+	// Prepare 100 sets of randomized counts
+	for _ = range 100 {
+		counts := make([]int, numTypes)
+		// Invididual counts can be up to 99 (N-1)
+		for i := range counts {
+			counts[i] = rand.IntN(100)
+		}
+		testData = append(testData, CountData{counts})
+	}
+
+	// Run subtest for each CountData struct, adding # of each animal type according to countsPerType, then checking getAnimalCount's return
+	for _, tt := range testData {
+		testname := fmt.Sprint(tt)
+
+		// Execute "subtest" on one data element from testData
+		t.Run(testname, func(t *testing.T) {
+			// Spin up fresh DB per test
+			db := initTestDBHelper(t)
+			defer db.Close()
+
+			// Insert data into db
+			tx, err := db.Begin()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Prepare a statement for repeated usage
+			stmt, err := tx.Prepare("INSERT INTO Animals(type, name) VALUES (?, ?)")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			defer stmt.Close()
+
+			// Insert animals into db according to specified counts
+			for i := range numTypes {
+				animalType := AnimalType(i)
+				for j := range tt.countsPerType[i] {
+					// Create generic name (e.g. dog0..dog4)
+					name := animalNames[animalType] + strconv.Itoa(j)
+					_, err = stmt.Exec(animalType, name)
+				}
+			}
+
+			// Commit the DB transaction
+			err = tx.Commit()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// For every specific type, and all types, query getAnimalCount and compare results
+			// Compile regex patterns for parsing count from returned msg
+			specific_re := regexp.MustCompile(`Number of ([a-zA-Z]+)s: ([0-9]+)`)
+			total_re := regexp.MustCompile(`Total number of animals: ([0-9]+)`)
+
+			for i := range numTypes {
+				animalTypeString := animalNames[AnimalType(i)]
+				resultMsg := getAnimalCount(db, animalTypeString)
+				matches := specific_re.FindStringSubmatch(resultMsg)
+				// For this pattern, the second capture group is the count
+				actualCount, err := strconv.Atoi(matches[2])
+				if err != nil {
+					t.Fatal(err)
+				}
+				expectedCount := tt.countsPerType[i]
+				if actualCount != expectedCount {
+					t.Errorf("Got %d, wanted %d count", actualCount, expectedCount)
+				}
+			}
+			resultMsg := getAnimalCount(db, "all animals")
+			matches := total_re.FindStringSubmatch(resultMsg)
+			// For this pattern, the first capture group is the count
+			actualCount, err := strconv.Atoi(matches[1])
+			if err != nil {
+				t.Fatal(err)
+			}
+			expectedCount := sumSlice(tt.countsPerType)
+			if actualCount != expectedCount {
+				t.Errorf("Got %d, wanted %d total count", actualCount, expectedCount)
+			}
+		})
 	}
 }
