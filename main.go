93c258cab0e97b72a2af877ac2d16fb16d1c1530
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2026-07-23 11:50:38 -0600

    increase size of text above each form for clearer separation

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"log"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
-	"log"
 	_ "modernc.org/sqlite"
 )
 
@@ -30,15 +32,18 @@ func main() {
 	// Initialize Animals table (if not yet created)
 	initDB(db)
 
-	// Create Fyne CanvasObject objects and corresponding labels for each UI element to add to the main window
+	// Create Fyne CanvasObject objects and corresponding subheading-sized labels for each UI element to add to the main window
 	// Pass in SQLite DB handle so widgets can access it
 	addSurrenderedAnimalText := widget.NewLabel("Add a New Surrendered Animal:")
+	addSurrenderedAnimalText.SizeName = theme.SizeNameSubHeadingText
 	addSurrenderedAnimalSection := makeSurrenderForm(db)
 
 	getAdoptedAnimalText := widget.NewLabel("Pick an Animal to Adopt:")
+	getAdoptedAnimalText.SizeName = theme.SizeNameSubHeadingText
 	getAdoptedAnimalSection := makeAdoptionForm(db)
 
 	animalCountText := widget.NewLabel("Check How Many Animals Are Up For Adoption:")
+	animalCountText.SizeName = theme.SizeNameSubHeadingText
 	animalCountSection := makeAnimalCountForm(db)
 
 	// Layout UI elements in vertical list
